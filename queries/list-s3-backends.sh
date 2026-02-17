8dcbcffa36c4e3a4e876c9514e8a493bf4d852f5
lbrealdev <lbrealdeveloper@gmail.com>
2026-02-17 20:27:03 +0100

    test: remove region from table output

--- queries/list-s3-backends.sh
+++ queries/list-s3-backends.sh
@@ -16,8 +16,8 @@ if [[ ! -d "$SEARCH_DIR" ]]; then
 fi
 
 # Print table header
-printf "%-40s %-50s %-50s %-15s\n" "FILE" "BUCKET" "KEY" "REGION"
-printf "%-40s %-50s %-50s %-15s\n" "----" "------" "---" "------"
+printf "%-40s %-50s %-50s\n" "FILE" "BUCKET" "KEY"
+printf "%-40s %-50s %-50s\n" "----" "------" "---"
 
 # Find and parse terraform files
 if ! find "$SEARCH_DIR" -name "*.tf" -type f -exec awk '
@@ -25,7 +25,6 @@ if ! find "$SEARCH_DIR" -name "*.tf" -type f -exec awk '
 in_block {
     if (match($0, /bucket[[:space:]]*=[[:space:]]*"([^"]+)"/, arr)) bucket=arr[1]
     if (match($0, /key[[:space:]]*=[[:space:]]*"([^"]+)"/, arr)) key=arr[1]
-    if (match($0, /region[[:space:]]*=[[:space:]]*"([^"]+)"/, arr)) region=arr[1]
 }
 /^\s*}\s*$/ && in_block {
     # Get parent directory and filename only
@@ -40,10 +39,9 @@ in_block {
     # Truncate long values for display (except bucket)
     if (length(display_file) > 40) display_file = substr(display_file, length(display_file)-39)
     if (length(key) > 50) key = substr(key, 1, 47) "..."
-    if (length(region) > 15) region = substr(region, 1, 12) "..."
     
-    printf "%-40s %-50s %-50s %-15s\n", display_file, bucket, key, region
-    in_block=0; bucket=""; key=""; region=""
+    printf "%-40s %-50s %-50s %-15s\n", display_file, bucket, key
+    in_block=0; bucket=""; key=""
 }
 ' {} + 2>/dev/null; then
     echo "No terraform files with S3 backends found"
